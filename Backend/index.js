64c6a068f1fc266b221174e7e45a26c33a4f2ea5
HARSHAL29072003 <harshal.gayakwad28@gmail.com>
2025-01-16 15:03:17 +0530

    ok done

--- Backend/index.js
+++ Backend/index.js
@@ -9,16 +9,24 @@ const Books = require("./routes/book");
 const Favourite = require("./routes/favourite");
 const Cart = require("./routes/cart");
 const Order = require("./routes/order");
-app.use(cors());
+
+const corsOptions = {
+    origin: "https://readersparadise.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    credentials: true, 
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-//routes
+
+// Routes
 app.use("/api/v1", User);
 app.use("/api/v1", Books);
 app.use("/api/v1", Favourite);
 app.use("/api/v1", Cart);
 app.use("/api/v1", Order);
 
-//creating port
-app.listen(process.env.PORT,() => {
+// Start server
+app.listen(process.env.PORT, () => {
     console.log(`Server Started at port ${process.env.PORT}`);   
 });
