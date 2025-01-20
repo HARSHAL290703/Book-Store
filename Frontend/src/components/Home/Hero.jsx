b34c0a1c243361b1a481c2bc2323bac7d2f4ba90
Harshal Gayakwad <160607933+HARSHAL290703@users.noreply.github.com>
2025-01-20 16:21:23 +0530

    Update Hero.jsx

--- Frontend/src/components/Home/Hero.jsx
+++ Frontend/src/components/Home/Hero.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="h-[75vh] flex flex-col md:flex-row items-center justify-center">
+    <div className="min-h-screen flex flex-col md:flex-row items-center justify-center">
       <div className="w-full mb-12 md:mb-0 lg:w-3/6 flex flex-col items-center lg:items-start justify-center">
         <h1 className="text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left">
           Discover Your Next Great Reads
@@ -13,18 +13,24 @@ const Home = () => {
           inspiration in our curated collection of books.
         </p>
         <div className="mt-8">
-          <Link to="/all-books" className="text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full">
+          <Link
+            to="/all-books"
+            className="text-yellow-100 text-md sm:text-xl lg:text-2xl font-semibold border border-yellow-100 px-4  py-3 hover:bg-zinc-800 rounded-full"
+          >
             Discover Books
           </Link>
         </div>
       </div>
-      <div className="w-full lg: w-3/6 h-auto lg:h-[100%] flex items-center justify-center">
-        <img src="https://i.pinimg.com/originals/2f/ab/f3/2fabf3ceb5a35b51a70e27137d56e4d2.gif
-" alt="hero  "style={{ borderRadius: "50px" , width: "600px", height: "auto"}} />
+      <div className="w-full lg: w-3/6 h-auto lg:h-[100%] flex items-center justify-center mb-10">
+        <img
+          src="https://i.pinimg.com/originals/2f/ab/f3/2fabf3ceb5a35b51a70e27137d56e4d2.gif
+"
+          alt="hero  "
+          style={{ borderRadius: "50px", width: "600px", height: "auto" }}
+        />
       </div>
     </div>
   );
 };
 
 export default Home;
-  
